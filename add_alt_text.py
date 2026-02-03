e0b64a2a81319a492d01254790a7df4ee4356cdf
Brian Gawalt <bgawalt@gmail.com>
2026-02-03 00:15:16 -0800

    Tkinter window now has text entry and a submit button which calls a useful callback to read that text entry.

--- add_alt_text.py
+++ add_alt_text.py
@@ -4,7 +4,8 @@ Usage, where `slides_db.db3` is the SQLite3 DB file being used:
 
   $ python add_alt_text.py slides_db.db3
 
-flurpity floo
+This is a janky Tkinter app that does not look or feel good, but it is getting
+the job done.
 """
 
 import base64
@@ -36,6 +37,11 @@ class Slide:
     width: int
     height: int
 
+    @classmethod
+    def from_row(cls, row: tuple[int, str, int, int]) -> 'Slide':
+        rid, jb64, w, h = row
+        return Slide(rid, jb64, w, h)
+
     def to_tk(self) -> ImageTk.PhotoImage:
         jpeg_bio = io.BytesIO(base64.b64decode(self.jpeg_base64))
         pil_img = Image.open(jpeg_bio, formats=['jpeg'])
@@ -44,20 +50,45 @@ class Slide:
 
 def main():
     root = tkinter.Tk()
-    
+
     db_filename = sys.argv[1]
     conn = sqlite3.connect(db_filename)
     cur = conn.cursor()
     cur.execute(_SELECT_ALTLESS_IMAGES)
-    row = cur.fetchone()
-    rid, jb64, w, h = row
-    s = Slide(rid, jb64, w, h)
-    stk = s.to_tk()
 
-    panel = tkinter.Label(root, image=stk)
-    panel.pack(side="top")
-    root.mainloop()
-    
+    row = cur.fetchone()
+    if not row:
+        print("Nothing to do here.")
+    slide = Slide.from_row(row)
+    print('Got slide', slide.rowid, slide.width, slide.height)
+    slide_tk = slide.to_tk()
+    slide_panel = tkinter.Label(root, image=slide_tk)
+    slide_panel.pack(side='top')
+
+    alt_text_panel = tkinter.Text(
+        root, width=80, height=6, font=("Arial", 15))
+    alt_text_panel.insert(1.0, "Enter alt text here.")
+    alt_text_panel.pack(side="top")
+
+    def submit():
+        print('submitted:')
+        print(alt_text_panel.get('1.0', 'end'))
+        alt_text_panel.delete('1.0', 'end')
+        row = cur.fetchone()
+        if not row:
+            alt_text_panel.insert('1.0', 'All done!!')
+            return
+        slide = Slide.from_row(row)
+        print('Got slide', slide.rowid, slide.width, slide.height)
+        slide_tk = slide.to_tk()
+        slide_panel.configure(image=slide_tk)
+        slide_panel.image = slide_tk
+        alt_text_panel.insert('1.0', 'Enter alt text here.')
+
+    submit_button = tkinter.Button(root, text='Submit', command=submit)
+    submit_button.pack(side='top')
+
+    root.mainloop()    
     conn.close()
 
 
