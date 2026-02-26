f46c26be555c28c0f25bc57609183a12d67abadd
Brian Gawalt <bgawalt@gmail.com>
2026-02-26 10:52:35 -0800

    Move tkinter functionality to a simpler just-show-the-image job called slide_viewer

---
+++ slide_viewer.py
@@ -0,0 +1,97 @@
+"""Pulls up a specific image by rowid in a TkInter window.
+
+Usage, where `slides_db.db3` is the SQLite3 DB file being used:
+
+  $ python slide_viewer.py slides_db.db3  id:1234
+"""
+
+import base64
+import dataclasses
+import io
+import sqlite3
+import sys
+import tkinter
+
+from PIL import Image
+from PIL import ImageTk
+
+
+_SELECT_IMAGE_QUERY = """
+    SELECT
+        rowid,
+        jpeg_base64,
+        collection,
+        alt_text
+    FROM slides
+    WHERE rowid = ?
+"""
+
+
+@dataclasses.dataclass(frozen=True)
+class Slide:
+    rowid: int
+    jpeg_base64: str
+    collection: str
+    alt_text: str
+
+    @classmethod
+    def from_row(cls, row: tuple[int, str, str, str]) -> 'Slide':
+        rid, jb64, col, alt = row
+        if not alt:
+            alt = '[no alt text on file]'
+        return Slide(rid, jb64, col, alt)
+
+    @property
+    def slide_id(self) -> str:
+        return f'{self.collection} :: {str(self.rowid)}'
+
+    def to_tk(self) -> ImageTk.PhotoImage:
+        jpeg_bio = io.BytesIO(base64.b64decode(self.jpeg_base64))
+        pil_img = Image.open(jpeg_bio, formats=['jpeg'])
+        curr_width, curr_height = pil_img.size
+        return ImageTk.PhotoImage(
+            pil_img.resize(
+                (round(curr_width * 2.5), round(curr_height * 2.5)),
+                Image.Resampling.LANCZOS)
+        )
+
+
+
+def main():
+    root = tkinter.Tk()
+
+    if len(sys.argv) != 3:
+        raise ValueError('Must supply two arguments.')
+    db_filename = sys.argv[1]
+    rowid_str = sys.argv[2]
+    if not rowid_str.startswith('id:'):
+        raise ValueError(f'rowid arg must start with "id:", got {rowid_str}')
+    rowid_num = int(rowid_str[3:])
+
+    conn = sqlite3.connect(db_filename)
+    cur = conn.cursor()
+
+    cur.execute(_SELECT_IMAGE_QUERY, (rowid_num,))
+    rows = cur.fetchall()
+    if len(rows) != 1:
+        raise ValueError(f'Multiple rows found for rowid {rowid_num}')
+    slide = Slide.from_row(rows[0])
+
+    slide_id_panel = tkinter.Label(root, text=slide.slide_id)
+    slide_id_panel.pack()
+    slide_tk = slide.to_tk()
+    slide_panel = tkinter.Label(root, image=slide_tk)
+    slide_panel.pack(side='top')
+
+    alt_text_panel = tkinter.Text(
+        root, width=80, height=6, font=('Arial', 15))
+    alt_text_panel.insert(1.0, slide.alt_text)
+    alt_text_panel.pack(side='top')
+    
+    root.geometry('%dx%d+%d+%d' % (1600, 1600, 800, 800))
+    root.mainloop()
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
